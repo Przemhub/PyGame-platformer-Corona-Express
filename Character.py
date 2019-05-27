c6401a43fb64c0722ae7836f0b48933be285002e
Przemhub <49333786+Przemhub@users.noreply.github.com>
2019-05-27 07:04:39 +0200

    Changes in animation
    Few changes to animation:
    - Seperated characted into 4 segments: head, torso, legs, weapon
    - added hitting animation
    - added function 'is_hitting' which checks whether the hit animaiton is active
    - added variable 'ARMED' to class 'player' which determines if character has weapon equiped

--- Character.py
+++ Character.py
@@ -8,7 +8,7 @@ class Boy:
         #Statistics
         self.init_stats()
         #Misc
-        H_files = listdir("Hero/Torso")
+        H_files = listdir("Hero/Head")
         T_files = listdir("Hero/Torso")
         L_files = listdir("Hero/Legs")
         W_files = listdir("Hero/Weaps")
@@ -19,13 +19,17 @@ class Boy:
         self.head = [pygame.image.load("Hero/Head/" + path) for path in H_files]
         self.torso = [pygame.image.load("Hero/Torso/" + path) for path in T_files]
         self.legs = [pygame.image.load("Hero/Legs/" + path) for path in L_files]
+
         self.weapon = [pygame.image.load("Hero/Weaps/" + path) for path in W_files]
-        self.headd = pygame.Rect(self.screen.get_width() / 2, self.screen.get_height()/2, 50, 40)
-        self.block = pygame.Rect(self.screen.get_width() / 2, self.screen.get_height()/2, 50, 4)
-        #Positioning Offsets [front,left,right,back]
-        self.left_offset = 2
-        self.right_offset = 4
-        self.back_offset = 6
+        self.Head_block = pygame.Rect(self.screen.get_width() / 2, self.screen.get_height()/2-20, 42, 57)
+        self.Torso_block = pygame.Rect(self.screen.get_width() / 2-6, self.screen.get_height()/2+23, 53, 78)
+        self.Legs_block = pygame.Rect(self.screen.get_width() / 2+4, self.screen.get_height() / 2 +87,
+                                      35, 44)
+        self.Weapon_block = pygame.Rect(self.screen.get_width() / 2 + 10, self.screen.get_height() / 2 +90,
+                                      42,30)
+        self.block = pygame.Rect(self.screen.get_width() / 2 + 100, self.screen.get_height() / 2 + 1230,
+                                        42, 57)
+
     def change_head(self, head):
         self.head_count = head
 
@@ -37,44 +41,48 @@ class Boy:
 
     def change_weapon(self, weapon):
         self.weapon = weapon
-
+    def relocate(self,x,y):
+        self.Head_block = self.Head_block.move(self.player_speed*x, self.player_speed*y)
+        self.Torso_block = self.Torso_block.move(self.player_speed*x, self.player_speed*y)
+        self.Legs_block = self.Legs_block.move(self.player_speed*x, self.player_speed*y)
+        self.Weapon_block = self.Weapon_block.move(self.player_speed*x, self.player_speed*y)
     def walk_front(self):
-        if (self.fps == 3):
+        if (self.fps == self.delay):
             self.fps = 0
             self.frames += 1
         self.fps += 1
-        self.frames = self.frames % 3
+        self.frames = self.frames % 4
         return (self.head[self.head_count + self.frames], self.torso[self.torso_count + self.frames], \
                 self.legs[self.legs_count + self.frames], self.weapon[self.weapon_count + self.frames])
 
     def walk_right(self):
-        if (self.fps == 3):
+        if (self.fps == self.delay):
             self.fps = 0
             self.frames += 1
         self.fps += 1
-        self.frames = self.frames % 2
+        self.frames = self.frames % 4
         return (self.head[self.right_offset + self.head_count + self.frames],
                 self.torso[self.right_offset + self.torso_count + self.frames], \
                 self.legs[self.right_offset + self.legs_count + self.frames],
                 self.weapon[self.right_offset + self.weapon_count + self.frames])
 
     def walk_left(self):
-        if (self.fps == 3):
+        if (self.fps == self.delay):
             self.fps = 0
             self.frames += 1
         self.fps += 1
-        self.frames = self.frames % 2
+        self.frames = self.frames % 4
         return (self.head[self.left_offset + self.head_count + self.frames],
                 self.torso[self.left_offset + self.torso_count + self.frames], \
                 self.legs[self.left_offset + self.legs_count + self.frames],
                 self.weapon[self.left_offset + self.weapon_count + self.frames])
 
     def walk_back(self):
-        if self.fps == 3:
+        if self.fps == self.delay:
             self.fps = 0
             self.frames += 1
         self.fps += 1
-        self.frames = self.frames % 3
+        self.frames = self.frames % 4
         return (self.head[self.back_offset + self.head_count + self.frames], self.torso[
             self.back_offset + self.torso_count + self.frames], \
                 self.legs[self.back_offset + self.legs_count + self.frames],
@@ -101,14 +109,43 @@ class Boy:
                 self.legs[self.back_offset + self.legs_count], self.weapon[self.back_offset + self.weapon_count])
 
     def hit_left(self):
-        return self.bang_left
+        if (self.fps == self.delay):
+            self.fps = 0
+            self.frames += 1
+        self.fps += 1
+        self.frames = self.frames % 2
+        return (self.head[self.head_count + self.frames + self.hit_offsetL],
+                self.torso[self.torso_count + self.frames + self.hit_offsetL], \
+                self.legs[self.legs_count + self.frames + self.hit_offsetL],
+                self.weapon[self.weapon_count + self.frames + self.hit_offsetL])
 
     def hit_right(self):
-        return self.bang_right
+        if (self.fps == self.delay):
+            self.fps = 0
+            self.frames += 1
+        self.fps += 1
+        self.frames = self.frames % 2
+        return (self.head[self.head_count + self.frames + self.hit_offsetR],
+                self.torso[self.torso_count + self.frames + self.hit_offsetR], \
+                self.legs[self.legs_count + self.frames + self.hit_offsetR],
+                self.weapon[self.weapon_count + self.frames + self.hit_offsetR])
 
     def hit_front(self):
-        return self.bang_front
-
+        if (self.fps == self.delay):
+            self.fps = 0
+            self.frames += 1
+        self.fps += 1
+        self.frames = self.frames % 2
+        return (self.head[self.head_count + self.frames + self.hit_offset], self.torso[self.torso_count + self.frames + self.hit_offset], \
+                self.legs[self.legs_count + self.frames + self.hit_offset], self.weapon[self.weapon_count + self.frames + self.hit_offset])
+    def hit_back(self):
+        if (self.fps == self.delay):
+            self.fps = 0
+            self.frames += 1
+        self.fps += 1
+        self.frames = self.frames % 2
+        return (self.head[self.head_count + self.frames + self.hit_offsetB], self.torso[self.torso_count + self.frames + self.hit_offsetB], \
+                self.legs[self.legs_count + self.frames + self.hit_offsetB], self.weapon[self.weapon_count + self.frames + self.hit_offsetB])
     def label_items(self):  # names of items in game
         self.HELMET1 = 0
         self.TORSO1 = 0
@@ -124,7 +161,16 @@ class Boy:
         self.weapon_count = 0
         self.frames = 0
         self.fps = 0
-
+        self.delay = 4;
+        self.player_speed = 5
+        # Positioning Offsets [front,left,right,back]
+        self.left_offset = 4
+        self.right_offset = 8
+        self.back_offset = 12
+        self.hit_offset = 16
+        self.hit_offsetL = 18
+        self.hit_offsetR = 20
+        self.hit_offsetB = 22
     def init_stats(self):
         self.HP = 100
         self.EXP = 0
@@ -132,3 +178,4 @@ class Boy:
         self.HEAD = 0
         self.TORSO = 0
         self.LEGS = 0
+        self.ARMED = False
--- Game.py
+++ Game.py
@@ -17,7 +17,8 @@ class Game:
         self.player = Boy(self.screen)
         self.action = [self.player.stay_front, self.player.walk_front, self.player.stay_right,
                           self.player.stay_left,self.player.walk_right, self.player.walk_left,\
-                          self.player.stay_back, self.player.walk_back]
+                          self.player.stay_back, self.player.walk_back,self.player.hit_front,\
+                          self.player.hit_left,self.player.hit_right,self.player.hit_back]
 
         self.move_i = 0
         self.stay_i=0
@@ -32,8 +33,11 @@ class Game:
         self.speed = 6
         self.fps = 30
         self.bullet = False
+        self.hit_flag=False
+        self.player_delay = 0
+
         #Basics
-        self.screen = pygame.display.set_mode((1600, 900))
+        self.screen = pygame.display.set_mode((1600,900),pygame.FULLSCREEN)
         self.screen=pygame.display.set_mode()
         self.clk = pygame.time.Clock()
     def draw(self):
@@ -43,11 +47,10 @@ class Game:
             self.shoot()
         (self.movement_head, self.movement_torso, self.movement_legs, self.movement_weapon) = self.action[
                 self.move_i]()
-
-        self.screen.blit(self.movement_head,self.player.block)
-        self.screen.blit(self.movement_torso, self.player.block)
-        self.screen.blit(self.movement_legs, self.player.block)
-        self.screen.blit(self.movement_weapon, self.player.block)
+        self.screen.blit(self.movement_legs, self.player.Legs_block)
+        self.screen.blit(self.movement_torso, self.player.Torso_block)
+        self.screen.blit(self.movement_head,self.player.Head_block)
+        self.screen.blit(self.movement_weapon, self.player.Weapon_block)
     def start(self):
         while True:
             for event in pygame.event.get():
@@ -61,34 +64,47 @@ class Game:
             pygame.display.flip()
             self.clk.tick(self.fps)
     def move(self):
-        self.move_i=self.stay_i
-        if not pygame.key.get_pressed()[pygame.K_SPACE]:
-            if pygame.key.get_pressed()[pygame.K_d] or pygame.key.get_pressed()[pygame.K_RIGHT]:
-                self.player.block.x+=self.speed
-                self.move_i = 4
-                self.stay_i = 2
-            if pygame.key.get_pressed()[pygame.K_a] or pygame.key.get_pressed()[pygame.K_LEFT]:
-                self.player.block.x-=self.speed
-                self.move_i = 5
-                self.stay_i = 3
-            if pygame.key.get_pressed()[pygame.K_s] or pygame.key.get_pressed()[pygame.K_DOWN]:
-                self.player.block.y+=self.speed
-                self.move_i = 1
-                self.stay_i = 0
-            if pygame.key.get_pressed()[pygame.K_w] or pygame.key.get_pressed()[pygame.K_UP]:
-                self.player.block.y-=self.speed
-                self.move_i = 7
-                self.stay_i = 6
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-            if self.stay_i == 0:
-                self.move_i = 8
-            elif self.stay_i == 3:
-                self.move_i = 9
-            elif self.stay_i == 2 :
-                self.move_i = 10
-            self.Bullet_x = self.player.block.x + 25
-            self.Bullet_y = self.player.block.y + 30
-            self.bullet = True
+
+        if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            sys.exit(0)
+        self.player.player_speed = 5
+        if pygame.key.get_pressed()[pygame.K_LSHIFT]:
+            self.player.player_speed = 10
+        if self.is_hitting():
+            self.player_delay+=1
+        else:
+            self.move_i=self.stay_i
+            self.player_delay = 0
+            self.hit_flag=False
+            if not pygame.key.get_pressed()[pygame.K_SPACE] or ~self.player.ARMED:
+                if pygame.key.get_pressed()[pygame.K_d] or pygame.key.get_pressed()[pygame.K_RIGHT]:
+                    self.move_i = 4
+                    self.stay_i = 2
+                    self.player.relocate(1,0);
+                if pygame.key.get_pressed()[pygame.K_a] or pygame.key.get_pressed()[pygame.K_LEFT]:
+                    self.move_i = 5
+                    self.stay_i = 3
+                    self.player.relocate(-1, 0);
+                if pygame.key.get_pressed()[pygame.K_s] or pygame.key.get_pressed()[pygame.K_DOWN]:
+                    self.move_i = 1
+                    self.stay_i = 0
+                    self.player.relocate(0, 1);
+                if pygame.key.get_pressed()[pygame.K_w] or pygame.key.get_pressed()[pygame.K_UP]:
+                    self.move_i = 7
+                    self.stay_i = 6
+                    self.player.relocate(0 , -1);
+            if pygame.key.get_pressed()[pygame.K_SPACE] and self.player.ARMED:
+                        self.hit_flag = True
+                        if self.stay_i == 0:
+                            self.move_i = 8
+                        elif self.stay_i == 3:
+                            self.move_i = 9
+                        elif self.stay_i == 2 :
+                            self.move_i = 10
+                        elif self.stay_i == 6:
+                            self.move_i = 11
+    def is_hitting(self): #if the animation of hitting is still active
+        return (self.hit_flag and self.player_delay<5)
     def shoot(self):
         self.Bullet = pygame.Rect(self.Bullet_x, self.Bullet_y, 5, 5)
         if self.move_i == 4 or self.stay_i == 2:
